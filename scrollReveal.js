a6baf15e1e02217c663c533a15db81666c344685
Eduardo Skrock <109317642+eduardoskrock@users.noreply.github.com>
2023-02-22 12:20:11 -0300

    Add files via upload

---
+++ scrollReveal.js
@@ -0,0 +1,92 @@
+window.sr = ScrollReveal({ reset: true})
+
+ScrollReveal().reveal( '#section1', {
+    delay: 900
+
+})
+
+ScrollReveal().reveal( 'header', {
+    delay: 900
+
+})
+
+
+ScrollReveal().reveal( '.btn', {
+    delay: 2400,
+    rotate:{
+        x:1000,
+        y:0,
+        z:0
+    }
+
+})
+
+
+
+
+ScrollReveal().reveal( '#section2', {
+    delay: 700
+
+})
+
+ScrollReveal().reveal( '.card-1', {
+    delay: 1600,
+    rotate:{
+        x:1500,
+        y:0,
+        z:0
+    }
+
+})
+
+ScrollReveal().reveal( '.card-2', {
+    delay: 1900,
+    rotate:{
+        x:1500,
+        y:0,
+        z:0
+    }
+
+})
+
+ScrollReveal().reveal( '.card-3', {
+    delay: 2200,
+    rotate:{
+        x:1500,
+        y:0,
+        z:0
+    }
+
+})
+
+ScrollReveal().reveal( '.card-4', {
+    delay: 2500,
+    rotate:{
+        x:1500,
+        y:0,
+        z:0
+    }
+
+})
+
+
+
+
+
+ScrollReveal().reveal( '#section3', {
+    delay: 700
+
+})
+
+ScrollReveal().reveal( '#section4', {
+    delay: 700
+
+})
+
+ScrollReveal().reveal( '#section5', {
+    delay: 700
+
+})
+
+
+
